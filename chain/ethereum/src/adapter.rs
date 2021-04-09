6ee329576b0523704f69755dc9746a3ee58415ea
Leonardo Yvens <leoyvens@gmail.com>
2021-04-09 11:18:39 -0300

    ethereum: Rebase web3, use upstream ethabi
    This is a rebase of https://github.com/graphprotocol/graph-node/pull/2362

--- chain/ethereum/src/adapter.rs
+++ chain/ethereum/src/adapter.rs
@@ -646,8 +646,8 @@ mod tests {
     use super::EthereumCallFilter;
 
     use graph::prelude::web3::types::Address;
+    use graph::prelude::web3::types::Bytes;
     use graph::prelude::EthereumCall;
-    use web3::types::Bytes;
 
     use std::collections::{HashMap, HashSet};
     use std::iter::FromIterator;
